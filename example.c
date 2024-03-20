84bd9923891432f70b554cbabbff58a4bec7684c
Thomas <80831811+Thomas470@users.noreply.github.com>
2024-03-20 18:37:56 -0300

    Update example.c

--- example.c
+++ example.c
@@ -1,14 +1,15 @@
 /*
 Estudante: Thomas Frentzel
-Curso: Bacharelado em Ciência da Computação
+Curso: Bacharelado em CiÃªncia da ComputaÃ§Ã£o
 */
 
 #include <stdio.h>
     #include <stdlib.h>
     #include "FreeRTOS.h"
     #include "task.h"
+    #include <basic_io.h>
 
-    // Definição das constantes para limites dos dados vitais
+    // DefiniÃ§Ã£o das constantes para limites dos dados vitais
     #define MIN_FREQ_CARD 20
     #define MAX_FREQ_CARD 140
     #define MIN_SAT_OXI 80
@@ -16,19 +17,19 @@ Curso: Bacharelado em Ciência da Computação
     #define MIN_TEMP 34
     #define MAX_TEMP 41
 
-    // Função para gerar valores aleatórios dentro de um intervalo
+    // FunÃ§Ã£o para gerar valores aleatÃ³rios dentro de um intervalo
     int generate_random(int min, int max) {
         return (rand() % (max - min + 1)) + min;
     }
 
-    // Tarefa para monitorar os batimentos cardíacos
+    // Tarefa para monitorar os batimentos cardÃ­acos
     void batimentos(void* pvParameters) {
         while (1) {
             int freq_card = generate_random(MIN_FREQ_CARD, MAX_FREQ_CARD);
-            printf("Batimentos: %d\n", freq_card);
+            vPrintStringAndNumber("Batimentos:\n", freq_card);
 
             if (freq_card < 50 || freq_card > 90) {
-                printf("Alerta! Batimento cardiaco alterado!\n");
+               vPrintStringAndNumber("Alerta! Batimento cardiaco alterado!\n", freq_card);
             }
 
             vTaskDelay(1000); // Aguarda 1 segundo
@@ -36,14 +37,14 @@ Curso: Bacharelado em Ciência da Computação
         vTaskDelete(NULL);
     }
 
-    // Tarefa para monitorar a saturação de oxigênio
+    // Tarefa para monitorar a saturaÃ§Ã£o de oxigÃªnio
     void saturacao(void* pvParameters) {
         while (1) {
             int sat_oxigenio = generate_random(MIN_SAT_OXI, MAX_SAT_OXI);
-            printf("Saturacao: %d\n", sat_oxigenio);
+            vPrintStringAndNumber("Saturacao:\n", sat_oxigenio);
 
             if (sat_oxigenio < 90) {
-                printf("Alerta! Saturacao de oxigenio baixa!\n");
+                vPrintStringAndNumber("Alerta! Saturacao de oxigenio baixa!\n", sat_oxigenio);
             }
 
             vTaskDelay(1000); // Aguarda 1 segundo
@@ -55,20 +56,20 @@ Curso: Bacharelado em Ciência da Computação
     void temperatura(void* pvParameters) {
         while (1) {
             int temperatura = generate_random(MIN_TEMP, MAX_TEMP);
-            printf("Temperatura: %d\n", temperatura);
+            vPrintStringAndNumber("Temperatura:\n", temperatura);
 
             if (temperatura < 35 || temperatura > 37.5) {
-                printf("Alerta! Temperatura alterada!\n");
+                vPrintStringAndNumber("Alerta! Temperatura alterada!\n",temperatura);
             }
 
-            vTaskDelay(1000);// Aguarda 1 segundo
+            vTaskDelay(1000);
         }
         vTaskDelete(NULL);
     }
 
     int main_(void) {
         
-        // Inicialização do FreeRTOS
+        // InicializaÃ§Ã£o do FreeRTOS
         xTaskCreate(batimentos, "Monitor de Batimentos", 1000, NULL, 1, NULL);
     
         xTaskCreate(saturacao, "Monitor Saturacao", 1000, NULL, 1, NULL);
